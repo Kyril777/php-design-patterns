1d28d37d924edbf72a5717fe749aef021d987b24
Kyril Jardinico <Kyril.Jay@gmail.com>
2021-03-21 23:21:05 -0400

    Create 02Factory_01.php

---
+++ 02Factory00/02Factory_01.php
@@ -0,0 +1,33 @@
+<?php
+// Factory
+interface Shape {
+    public function draw();
+}
+
+class Rectangle implements Shape {
+    public $x;
+    public $y;
+    public $a;
+    public $b;
+
+
+    public function draw() {
+        echo "Drawing a rectangle";
+    }
+}
+
+function drawStuff(Shape $shape) {
+    $shape->draw();
+}
+
+$shape new Rectangle();
+drawStuff($shape);
+
+
+/*
+Source: Easy Learn Tutorial
+https://www.youtube.com/user/easylearntutorial
+The Factory helps de-couple your code, so that the client code doesn't need to worry about how to construct objects. 
+This pattern isolates the code to test different things or have the logic in one place. 
+In testing, the Factory pattern creates a mock class that prevents us from assuming that the class also works. 
+*/
