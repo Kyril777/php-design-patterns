e4468423e740a1e2c95b0b8ae32eb5209e05c3f3
Kyril Jardinico <Kyril.Jay@gmail.com>
2021-03-21 23:27:31 -0400

    Create 03Strategy_02.php

---
+++ 03Strategy00/03Strategy_02.php
@@ -0,0 +1,49 @@
+<?php
+// Strategy
+interface SortStrategy {
+    public function sort();
+}
+
+class QuickSort {
+    private $data;
+
+    public function __construct(Array $data) {
+        $this->data = $data;
+    }
+
+    public function sort() {
+        // ...
+    }
+
+}
+
+class MergeSort {
+    private $data;
+
+    public function __construct(Array $data) {
+        $this->data = $data;
+    }
+
+    public function sort() {
+        // ...
+    }
+}
+
+function sort(Array &$data) {
+    if (count($data) > 20) {
+        $tempData = new QuickSort($data);
+    } else {
+        $tempData = new MergeSort($data);      
+    }
+}
+
+$data = array(7, 2, 5, 9, 6, 5, 4, 1, 2, 3, 2, 77, 25);
+sort($data);
+
+
+/*
+Source: Easy Learn Tutorial
+https://www.youtube.com/user/easylearntutorial
+The Strategy allows you to select more than one algorithm at a time. 
+Between Quickstart and Megesort, the script can implement either depending on load and efficiency.
+*/
