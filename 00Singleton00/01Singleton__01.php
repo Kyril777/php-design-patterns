3523fb80cfd9b61f65213faaefe4f408b94d0e87
Kyril Jardinico <Kyril.Jay@gmail.com>
2021-03-21 23:19:17 -0400

    Create 01Singleton__01.php

---
+++ 00Singleton00/01Singleton__01.php
@@ -0,0 +1,33 @@
+<?php
+// Singleton
+class Database {
+    public static $instance;
+
+    public static function getInstance() {
+        if(!isset(Database::$instance))
+            Database::$instance = new Database();
+
+        return Database::$instance;
+    }
+
+    public function __construct() {
+        /* PRIVATE. */
+    }
+
+    public function getQuery() {
+        return "SELECT * FROM our_table";
+    }
+}
+
+$db = Database::getInstance();
+echo $db->getQuery();
+
+
+/*
+Source: Easy Learn Tutorial
+https://www.youtube.com/user/easylearntutorial
+Singleton allows you to have one and only one instance of a class in your project. 
+The Singleton is sometimes known as just a fancy term for the global variable -- it just uses a class instead.
+As global variables are corruptible, the class is also susceptible as well. 
+So in order to maintain the integrity of the program, it must only produce one instance of a class.
+*/
